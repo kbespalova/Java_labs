5ace034ddffcfff853afe579e6c83ab61f5451e0
kbespalova <bespalovakaterina11@gmail.com>
2023-06-02 11:48:31 +0200

    Commit

---
+++ lab_3/src/model/Human.java
@@ -0,0 +1,20 @@
+package model;
+
+public abstract class Human {
+    protected String firstName;
+    protected String lastName;
+    protected Sex sex;
+
+    public Human(String firstName, String lastName, Sex sex)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.sex = sex;
+    }
+
+    public abstract String getRole();
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+}
